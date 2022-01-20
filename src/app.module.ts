3bb11b35ee3a92af333c75feef2067a88f0da512
Generic Octopus <genericoctopus@gmail.com>
2022-01-20 15:51:12 -0500

    update

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { PostModule } from './posts/posts.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    PostModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
